9dcfea777f4e0651713dda4ca1152a085ceba6d4
ruman23 <mail2ruman.i@gmail.com>
2023-07-07 17:34:59 -0500

    add unit test cases

--- ShoppingList/ShoppingListViewModel.swift
+++ ShoppingList/ShoppingListViewModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+class ShoppingListViewModel {
+    struct Item {
+        let title: String
+        let detail: String
+        var hasCheckmark: Bool
+    }
+    
+    private(set) var items: [Item] = []
+    
+    func loadData() {
+        items = [
+            Item(title: "Item A", detail: "50 lb", hasCheckmark: true),
+            Item(title: "Item B", detail: "1", hasCheckmark: false),
+            Item(title: "Item C", detail: "2 lb", hasCheckmark: true)
+        ]
+    }
+    
+    var numberOfItems: Int {
+        return items.count
+    }
+    
+    func item(at index: Int) -> Item {
+        return items[index]
+    }
+    
+    func deleteItem(at index: Int) {
+        items.remove(at: index)
+    }
+    
+    func addItem(_ item: Item) {
+        items.insert(item, at: 0)
+    }
+    
+    func toggleCheckmark(at index: Int) {
+        items[index].hasCheckmark.toggle()
+    }
+}
--- ShoppingListTests/ShoppingListTests.swift
+++ ShoppingListTests/ShoppingListTests.swift
@@ -9,28 +9,57 @@ import XCTest
 @testable import ShoppingList
 
 final class ShoppingListTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var viewModel: ShoppingListViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = ShoppingListViewModel()
+        viewModel.loadData()
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+    
+    func testLoadData() {
+        XCTAssertEqual(viewModel.numberOfItems, 3)
+    }
+    
+    func testDeleteItem() {
+        viewModel.deleteItem(at: 0)
+        XCTAssertEqual(viewModel.numberOfItems, 2)
+    }
+    
+    func testItem() {
+        let item = viewModel.item(at: 0)
+        XCTAssertEqual(item.title, "Item A")
+        XCTAssertEqual(item.detail, "50 lb")
+        XCTAssertTrue(item.hasCheckmark)
+    }
+    
+    func testToggleCheckmark() {
+        viewModel.toggleCheckmark(at: 0)
+        let item = viewModel.item(at: 0)
+        XCTAssertFalse(item.hasCheckmark)
+    }
+    
+    func testAddItem() {
+        let newItem = ShoppingListViewModel.Item(title: "Item D", detail: "10 lb", hasCheckmark: false)
+        viewModel.addItem(newItem)
+        
+        XCTAssertEqual(viewModel.numberOfItems, 4)
+        
+        let item = viewModel.item(at: 0)
+        XCTAssertEqual(item.title, "Item D")
+        XCTAssertEqual(item.detail, "10 lb")
+        XCTAssertFalse(item.hasCheckmark)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testReload() {
+        viewModel.loadData()
+        XCTAssertEqual(viewModel.numberOfItems, 3)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
